3cd19bc5201f8b3ca439cadcb978527c55a84adf
jumshoos <jumsheed.k@gmail.com>
2019-04-27 22:40:08 -0400

    Bubble Sort
    Bubble Sort

---
+++ BubbleSrt.py
@@ -0,0 +1,11 @@
+def bSort(lst):
+
+    for _ in range(len(lst)):
+        for i in range(len(lst) - 1):
+            if lst[i] > lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+
+    return lst
+
+
+print(bSort([6, 1, 3, 2, 5, 4]))
